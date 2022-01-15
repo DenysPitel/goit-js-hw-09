920e18975d46cc2d9e2657e6ba951234d0476852
Denys Pitel <nk120788pda@gmail.com>
2022-01-16 01:17:53 +0200

    finished_task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    body: document.querySelector('body')
+};
+
+let timerId = null;
+
+refs.startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    function getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+});
+
+refs.stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+});
 
